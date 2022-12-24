b23b5515b97ae6140928046d93b01ed5859cdb9e
philcgibson <philg@stradx.com>
2022-12-24 13:50:13 +0000

    Added RadixWiki.com Repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "radixwiki", // Usually your GitHub org/user name.
-  projectName: "radixwiki", // Usually your repo name.
+  organizationName: "Stradx", // Usually your GitHub org/user name.
+  projectName: "RadixWiki.com", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -36,8 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Stradx/RadixWiki.com/tree/main/",
         },
         blog: false,
         // blog: {
